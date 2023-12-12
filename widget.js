7b62f33669f2487a438cb9cec579fcf8a01fb864
Jaden <jadenmendoza02@gmail.com>
2023-12-11 21:36:54 -0800

    rework

--- widget.js
+++ widget.js
@@ -2,29 +2,24 @@ document.addEventListener('DOMContentLoaded', function() {
     const apiUrl = 'https://api.weather.gov/points/32.8801,-117.2340';
 
     fetch(apiUrl)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-            return response.json();
-        })
+        .then(response => response.json())
         .then(data => {
             const forecastUrl = data.properties.forecast;
             return fetch(forecastUrl);
         })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-            return response.json();
-        })
+        .then(response => response.json())
         .then(data => {
             const currentWeather = data.properties.periods[0];
             document.getElementById('temperature').textContent = `Temperature: ${currentWeather.temperature} ${currentWeather.temperatureUnit}`;
             document.getElementById('conditions').textContent = `Conditions: ${currentWeather.shortForecast}`;
+            document.getElementById('wind').textContent = `Wind Speed: ${currentWeather.windSpeed}`;
+            document.getElementById('humidity').textContent = `Humidity: ${currentWeather.humidity}%`;
+            // Set the weather icon src
+            // document.getElementById('weather-icon').src = 'path/to/local/icon';
+            // document.getElementById('weather-icon').style.display = 'block';
         })
         .catch(error => {
+            document.getElementById('weather-content').innerHTML = '<p>Current Weather Conditions Unavailable</p>';
             console.error('Error fetching weather data:', error);
-            document.getElementById('temperature').textContent = 'Error loading weather data.';
         });
 });
