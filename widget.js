3aefb54d4cca5d855a031808d60451401411a0ad
Jaden <jadenmendoza02@gmail.com>
2023-12-11 21:59:47 -0800

    final

--- widget.js
+++ widget.js
@@ -10,10 +10,8 @@ document.addEventListener('DOMContentLoaded', function() {
         .then(response => response.json())
         .then(data => {
             const currentWeather = data.properties.periods[0];
-            document.getElementById('temperature').textContent = `Temperature: ${currentWeather.temperature} ${currentWeather.temperatureUnit}`;
-            document.getElementById('conditions').textContent = `Conditions: ${currentWeather.shortForecast}`;
-            document.getElementById('wind-speed').textContent = `Wind: ${currentWeather.windSpeed}`;
-            document.getElementById('humidity').textContent = `Humidity: ${currentWeather.humidity}%`;
+            document.getElementById('temperature').textContent = `${currentWeather.temperature} ${currentWeather.temperatureUnit}`;
+            document.getElementById('conditions').textContent = currentWeather.shortForecast;
             document.getElementById('weather-icon').src = currentWeather.icon;
             document.getElementById('weather-icon').style.display = 'inline';
         })
