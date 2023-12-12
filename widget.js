e9674aa5a3ecfa8a27ed61405f042ffd4fc85066
Jaden <jadenmendoza02@gmail.com>
2023-12-11 21:50:18 -0800

    style edit

--- widget.js
+++ widget.js
@@ -10,13 +10,10 @@ document.addEventListener('DOMContentLoaded', function() {
         .then(response => response.json())
         .then(data => {
             const currentWeather = data.properties.periods[0];
-            document.getElementById('temperature').textContent = `Temperature: ${currentWeather.temperature} ${currentWeather.temperatureUnit}`;
-            document.getElementById('conditions').textContent = `Conditions: ${currentWeather.shortForecast}`;
-            document.getElementById('wind').textContent = `Wind Speed: ${currentWeather.windSpeed}`;
-            document.getElementById('humidity').textContent = `Humidity: ${currentWeather.humidity}%`;
-            // Set the weather icon src
-            // document.getElementById('weather-icon').src = 'path/to/local/icon';
-            // document.getElementById('weather-icon').style.display = 'block';
+            document.getElementById('temperature').textContent = `${currentWeather.temperature} ${currentWeather.temperatureUnit}`;
+            document.getElementById('conditions').textContent = currentWeather.shortForecast;
+            document.getElementById('weather-icon').src = currentWeather.icon;
+            document.getElementById('weather-icon').style.display = 'inline';
         })
         .catch(error => {
             document.getElementById('weather-content').innerHTML = '<p>Current Weather Conditions Unavailable</p>';
